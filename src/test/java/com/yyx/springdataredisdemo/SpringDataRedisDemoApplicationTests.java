ed1f43da6c3340b3ccfa5aa1cdf3e5bec373b386
userName <userEmail>
2025-09-02 07:28:31 +0800

    add:注入RedisTemplate

--- src/test/java/com/yyx/springdataredisdemo/SpringDataRedisDemoApplicationTests.java
+++ src/test/java/com/yyx/springdataredisdemo/SpringDataRedisDemoApplicationTests.java
@@ -1,10 +1,14 @@
 package com.yyx.springdataredisdemo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
 class SpringDataRedisDemoApplicationTests {
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @Test
     void contextLoads() {
