e56e757af354f8c09199f6ef29f7062b10be133f
userName <userEmail>
2025-09-02 07:02:50 +0800

    add:创建SpringDataRedis项目

---
+++ src/test/java/com/yyx/springdataredisdemo/SpringDataRedisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yyx.springdataredisdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRedisDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
