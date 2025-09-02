571d1097d805c1c609ffcd7922ed7a21389cc9ee
userName <userEmail>
2025-09-02 09:32:28 +0800

    add:手动处理json

--- src/test/java/com/yyx/springdataredisdemo/RedisDemoApplicationTests.java
+++ src/test/java/com/yyx/springdataredisdemo/RedisDemoApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
-class SpringDataRedisDemoApplicationTests {
+class RedisDemoApplicationTests {
     @Autowired
 //    private RedisTemplate redisTemplate;
     private RedisTemplate<String, Object> redisTemplate;
---
+++ src/test/java/com/yyx/springdataredisdemo/RedisStringTests.java
@@ -0,0 +1,40 @@
+package com.yyx.springdataredisdemo;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yyx.springdataredisdemo.redis.pojo.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+@SpringBootTest
+class RedisStringTests {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Test
+    void testString() {
+        stringRedisTemplate.opsForValue().set("name", "yyx2");
+        System.out.println(stringRedisTemplate.opsForValue().get("name"));
+//        stringRedisTemplate.opsForValue().set("age", 18);
+//        System.out.println(stringRedisTemplate.opsForValue().get("age"));
+    }
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+    @Test
+    void testSaveUser() throws JsonProcessingException {
+        //创建对象
+        User user = new User("yyx", 23);
+        //手动序列化
+        String json = mapper.writeValueAsString( user);
+        //写入数据
+        stringRedisTemplate.opsForValue().set("user:200", json);
+        //获取数据
+        String jsonUser = stringRedisTemplate.opsForValue().get("user:200");
+        //手动反序列化
+        User object = mapper.readValue(jsonUser, User.class);
+        System.out.println(object);
+    }
+}
